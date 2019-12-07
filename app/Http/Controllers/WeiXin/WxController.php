9913f1497e7103b4744fdd4a80614c970ce0ae4d
root <2877503663@qq.com>
2019-12-07 05:42:00 +0000

    验证微信接入

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -9,6 +9,21 @@ class WxController extends Controller
 {
     public function wechat()
     {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $echostr = $_GET["echostr"];
         
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+        
+        if( $tmpStr == $signature ){
+            echo $echostr;
+        }else{
+            die "not ok";
+        }
     }
 }

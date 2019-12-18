371caaac14ee07b128b79826a4e0f98bdfbe6220
root <2877503663@qq.com>
2019-12-18 20:22:56 +0800

    获取用户基本信息

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -12,7 +12,9 @@ class VoteController extends Controller
         $code = $_GET['code'];
 
         // 获取access_token
-        $this->getAccessToken($code);
+        $data = $this->getAccessToken($code);
+        // 获取用户信息
+        $user_info = $this->getUserInfo($data['access_token'],$data['openid']);
     }
 
     /**
@@ -22,7 +24,17 @@ class VoteController extends Controller
     {
         $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
         $json_data = file_get_contents($url);
-        $data = json_decode($json_data,true);
-        echo '<pre>';print_r($data);echo '</pre>';
+        return json_decode($json_data,true);
+    }
+
+    /**
+     * 获取用户基本信息
+     */
+    protected function getUserInfo($access_token,$openid)
+    {
+        $url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+        $json_data = file_get_contents($url);
+        $user_info = json_decode($json_data,true);
+        echo '<pre>';print_r($user_info);echo '</pre>';
     }
 }

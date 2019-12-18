9d7cc66230ebf6827a555c8c0747c29393148d75
root <2877503663@qq.com>
2019-12-18 21:06:53 +0800

    投票

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -8,13 +8,18 @@ class VoteController extends Controller
 {
     public function index()
     {
-        echo '<pre>';print_r($_GET);echo '</pre>';
+        // echo '<pre>';print_r($_GET);echo '</pre>';
         $code = $_GET['code'];
 
         // 获取access_token
         $data = $this->getAccessToken($code);
         // 获取用户信息
         $user_info = $this->getUserInfo($data['access_token'],$data['openid']);
+
+        // 处理业务逻辑
+        $redis_key = 'vote';
+        $number = Redis::incr($redis_key);
+        echo "投票成功,当前拍数: ".$number;
     }
 
     /**
@@ -34,7 +39,11 @@ class VoteController extends Controller
     {
         $url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
         $json_data = file_get_contents($url);
-        $user_info = json_decode($json_data,true);
-        echo '<pre>';print_r($user_info);echo '</pre>';
+        $data = json_decode($json_data,true);
+        if(isset($data['errcode'])){
+            // 错误处理
+            die('嘿!兄弟出错了 40001');  //40001标识获取用户信息失败
+        }
+        return $data;
     }
 }

4c1708ad2d3c0b42705e9b7f1d3edc1c5268c94a
root <2877503663@qq.com>
2019-12-18 20:12:57 +0800

    bug

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -9,5 +9,20 @@ class VoteController extends Controller
     public function index()
     {
         echo '<pre>';print_r($_GET);echo '</pre>';
+        $code = $_GET['code'];
+
+        // 获取access_token
+        $this->getAccessToken($code);
+    }
+
+    /**
+     * 根据code 获取access_token
+     */
+    protected function getAccessToken($code)
+    {
+        $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
+        $json_data = file_get_contents($url);
+        $data = json_decode($json_data,true);
+        echo '<pre>';print_r($data);echo '</pre>';
     }
 }

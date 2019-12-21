bc59a5268383eef1e332b256951b07286c87a484
root <2877503663@qq.com>
2019-12-21 15:27:47 +0800

    富文本

--- resources/views/goods/detail.blade.php
+++ resources/views/goods/detail.blade.php
@@ -367,10 +367,11 @@
 		<div class="container">
 			<div class="shop-single">
 				<img src="/storage/{{$goods->img}}" alt="">
-				<h5>Fashion Men's</h5>
-				<div class="price">$20 <span>$28</span></div>
+				<h5>{{$goods->goods_name}}</h5>
+				<div class="price">${{$goods->price}} <span>${{$goods->price}}</span></div>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam eaque in non delectus, error iste veniam commodi mollitia, officia possimus, repellendus maiores doloribus provident. Itaque, ab perferendis nemo tempore! Accusamus</p>
-				<button type="button" class="btn button-default">ADD TO CART</button>
+				{!!$goods->desc!!}
+				<button type="button" class="btn button-default">添加到购物车</button>
 			</div>
 			<div class="review">
 					<h5>1 reviews</h5>
@@ -408,7 +409,7 @@
 								<textarea name="textarea-message" id="textarea1" cols="30" rows="10" class="materialize-textarea" class="validate" placeholder="YOUR REVIEW"></textarea>
 							</div>
 							<div class="form-button">
-								<div class="btn button-default">POST REVIEW</div>
+								<div class="btn button-default">发表评论</div>
 							</div>
 						</form>
 					</div>
